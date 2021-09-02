ec2fd9d32b78cc001b74fc692db25d7dc06cdccc
edpaulsen <ed@paulsenhome.com>
2021-09-02 15:37:26 -0500

    git fixed

---
+++ frontend/src/data.js
@@ -0,0 +1,97 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Basic Subscription - Free Subscription',
+            category: 'Subscription',
+            image: '/images/product.png',
+            price: 0,
+            countInStock: 0,
+            brand: 'HIPAA Forms',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Free Version of HIPAA forms allowing for 1 form and 25 submissions per month',
+          },
+
+          {
+            _id: '2',
+            name: 'Standard Monthly Subscription',
+            category: 'Subscription',
+            image: '/images/product.png',
+            price: 55,
+            countInStock: 20,
+            brand: 'HIPAA Forms',
+            rating: 2.5,
+            numReviews: 100,
+            description: 'Standard HIPAA forms monthly subscription.  Unlimited forms and Unlimited form submissions',
+          },
+
+          {
+            _id: '3',
+            name: 'Standard Monthly with File Upload',
+            category: 'Subscription',
+            image: '/images/product.png',
+            price: 85,
+            countInStock: 20,
+            brand: 'HIPAA Forms',
+            rating: 1.5,
+            numReviews: 5555,
+            description: 'Standard HIPAA forms monthly subscription with File Upload.  Unlimited forms, Unlimited form submissions, & unlimited file uploads',
+          },
+
+          {
+            _id: '4',
+            name: 'Standard Quarterly Subscription',
+            category: 'Subscription',
+            image: '/images/product.png',
+            price: 160,
+            countInStock: 20,
+            brand: 'HIPAA Forms',
+            rating: 5,
+            numReviews: 10,
+            description: 'Standard HIPAA forms quarterly subscription.  Unlimited forms and Unlimited form submissions',
+          },
+
+          {
+            _id: '5',
+            name: 'Standard Quarterly Subscription with File Upload',
+            category: 'Subscription',
+            image: '/images/product.png',
+            price: 240,
+            countInStock: 20,
+            brand: 'HIPAA Forms',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Standard HIPAA forms quarterly subscription with File Upload.  Unlimited forms, Unlimited form submissions, & unlimited file uploads',
+          },
+
+          {
+            _id: '6',
+            name: 'Standard Yearly Subscription',
+            category: 'Subscription',
+            image: '/images/product.png',
+            price: 600,
+            countInStock: 20,
+            brand: 'HIPAA Forms',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Standard HIPAA forms yearly subscription.  Unlimited forms and Unlimited form submissions',
+          },
+
+          {
+            _id: '7',
+            name: 'Standard Yearly Subscription with File Upload',
+            category: 'Subscription',
+            image: '/images/product.png',
+            price: 900,
+            countInStock: 20,
+            brand: 'HIPAA Forms',
+            rating: 5,
+            numReviews: 10,
+            description: 'Standard HIPAA forms yearly subscription with File Upload.  Unlimited forms, Unlimited form submissions, & unlimited file uploads',
+          }
+
+    ],
+};
+
+export default data;
